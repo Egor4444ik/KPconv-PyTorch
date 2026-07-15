a17c93597e1b9a49aa3b61a87806c05a15b4dde4
Егор Смирнов <egor4e4ik@gmail.com>
2026-07-15 15:22:58 +0300

    area skipping replacing 3

--- Data/oneLaszToManyTxt.py
+++ Data/oneLaszToManyTxt.py
@@ -96,7 +96,7 @@ class lasToTxt:
 
                 annot_dir = self.base_path / area_name / region_name / 'Annotations'
 
-                if area_name in os.listdir(annot_dir):
+                if area_name in os.listdir(self.S3DIS_path):
                         if len(annot_dir)>0:
 
                             print(f'  {area_name}, already exists')
